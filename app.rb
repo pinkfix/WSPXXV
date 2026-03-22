4ee7a199436a74bbbc0a824e339a87aa8c0a80d8
pinkfix <robin@willim.se>
2026-03-22 17:08:30 +0100

    Lade till forumet
    Fixade ett chattsystem där man kan ta bort egna meddelanden, andras om man är admin, skriva & starta trådar om man är inloggad och fortfarande se allt om man är utloggad.

--- app.rb
+++ app.rb
@@ -9,22 +9,6 @@ enable :sessions
 # BEFORE-BLOCK
 
 # Definiera ett before-block som kollar om användaren är inloggad och admin
-before do
- # Lista alla begränsade routes
- restricted_paths = ['/private', '/admin/*', '/settings']
-
- # Om användaren inte är inloggad och försöker komma åt en begränsad sökväg,
- # omdirigera dem till inloggningssidan.	Här har session[:logged_in satts till “true” vid inloggning. 
- if session[:logged_in] && restricted_paths.include?(request.path_info)   #alternativt session[:id] != nil...
-   redirect '/login'
- end
-
- # Om användaren är inloggad men inte är en administratör och försöker komma åt en administratörssökväg,
- # omdirigera dem till startsidan. Här har session[:admin] satts till “true” vid inloggningen.
- if session[:logged_in] && !session[:admin] && request.path_info == '/admin'
-   redirect '/'
- end
-end
 
 
 
@@ -34,17 +18,28 @@ end
 
 #Helper functions
 
-def getDB(database)
-  db = SQLite3::Database.new(database)
-  db.results_as_hash = true
-  return db
+before do
+  def getDB(database)
+    db = SQLite3::Database.new(database)
+    db.results_as_hash = true
+    return db
+  end
+
+  def backgroundCheck(user_id)
+    db = getDB("db/railed.db")
+    if db.execute("SELECT lvl FROM users WHERE id=?",user_id).first["lvl"] == 0
+      return false
+    else
+      return true
+    end
+  end
 end
 
 
 # FUNKTIONER RELATERADE TILL ANVÄNDARSYSTEMET
 
 get('/register') do
-  slim(:register)
+  slim(:signup)
 end
 
 post('/register') do
@@ -52,25 +47,30 @@ post('/register') do
   user = params["user"]
   pwd = params["pwd"]
   pwd_confirm = params["pwd_confirm"]
-  lvl = params["admin"]
+  lvl = 0
+  session[:admin] = 0
+  if params["admin"]
+    lvl = 1
+    session[:admin] = 1
+  end
+
   
-  result = db.execute("SELECT id FROM user WHERE username=?",user)
+  result = db.execute("SELECT id FROM users WHERE username=?",user)
 
   if result.empty?
     if pwd == pwd_confirm
       pwd_digest = BCrypt::Password.create(pwd)
-      db.execute("INSERT INTO users(username, pwd_digest, lvl) VALUES (?,?,?)",[user,pwd_digest, lvl])
-      if lvl
-        session[:admin] = true
-      end
       session[:logged_in] = true
       user_id = db.execute("SELECT id FROM users WHERE username=?", user)
       session[:user_id] = user_id
+      db.execute('INSERT INTO users (username, pwd_digest, lvl) VALUES (?,?,?)',[user, pwd_digest, lvl])
       redirect('/hub')
     else
+      p "password matchar ej"
       redirect('/register') #pwd != pwd2
     end
   else
+    p "usernamn taget"
     redirect('/register') #username redan taget
   end
 end
@@ -87,21 +87,33 @@ post('/login') do
   result = db.execute("SELECT id,pwd_digest,lvl FROM users WHERE username=?",user)
 
   if result.empty?
+    p "Användare finnes ej"
     redirect('/login') #Fel användarnamn
   end
   puts result
   user_id = result.first["id"]
   pwd_digest = result.first["pwd_digest"]
   if BCrypt::Password.new(pwd_digest) == pwd
-    if result.first["lvl"]
+    p result.first["lvl"]
+    if result.first["lvl"] == 1
       session[:admin] = true
+    else
+      session[:admin] = false
     end
     session[:logged_in] = true
     session[:user_id] = user_id
     redirect('/hub')
   else
+    p "Fel lösenord"
     redirect('/login') #Fel lösenord
   end
+end
+
+post('/logout') do
+  session[:logged_in] = false
+  session[:admin] = false
+  session[:user_id] = 0
+  redirect('/hub')
 end
 
 # HUB-GETS & POSTS
@@ -113,10 +125,53 @@ get('/hub') do
   slim(:hub)
 end
 
-# HÄR BÖRJAR TINDERRELATERADE GETS OCH POSTS
+#TINDERRELATERADE GETS OCH POSTS
 get('/tinder') do
   db = getDB("db/railed.db")
   @id = session[:user_id]
+  p @id
   @name =  db.execute("SELECT username FROM users WHERE id=?", @id).first["username"]
-  slim(:tinder)
+  slim(:tinderhub)
+end
+
+#Forumrelaterade Gets & Posts
+get('/forum') do
+  db = getDB("db/railed.db")
+  @forumen = db.execute('SELECT * FROM forums')
+  slim(:forumhub)
+end
+
+get('/forum/:id') do
+  @id = params[:id]
+  db = getDB("db/railed.db")
+  @user_name = db.execute("SELECT username FROM users WHERE id =?", session[:user_id]).first["username"]
+  @rubbe = db.execute("SELECT rubrik FROM forums WHERE id=?",@id).first["rubrik"]
+  @chatten = db.execute("SELECT messages.id,username,message FROM messages LEFT JOIN users on messages.user = users.id WHERE forum = ?", @id)
+  slim(:forum)
+end
+
+post('/createforum') do
+  rubr = params[:rub]
+  db = getDB("db/railed.db")
+  db.execute("INSERT INTO forums (rubrik) VALUES (?)", rubr)
+  id = db.execute("SELECT id FROM forums WHERE rubrik = ?", rubr).first["id"]
+  redirect("/forum/#{id}")
+end
+
+post('/chatta/:id') do
+  id = params[:id]
+  mess = params[:meddelande]
+  uid = session[:user_id]
+  db = getDB("db/railed.db")
+  db.execute('INSERT INTO messages (forum,user,message) VALUES (?,?,?)',[id,uid,mess])
+  redirect("/forum/#{id}")
+end
+
+post('/radera/:fid/:id') do
+  mid = params[:id]
+  p mid
+  db = getDB("db/railed.db")
+  fid = params[:fid]
+  db.execute("DELETE FROM messages WHERE id = ?",mid)
+  redirect("/forum/#{fid}")
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -17,6 +17,7 @@ end
 def drop_tables(db)
   db.execute('DROP TABLE IF EXISTS users')
   db.execute('DROP TABLE IF EXISTS forums')
+  db.execute('DROP TABLE IF EXISTS messages')
 end
 
 def create_tables(db)
@@ -24,17 +25,26 @@ def create_tables(db)
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               username TEXT NOT NULL,
               pwd_digest TEXT NOT NULL,
-              lvl BOOLEAN)')
-  #db.execute('CREATE TABLE forums (
-   #           id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #          name TEXT NOT NULL)')
+              lvl INTEGER)')
+  db.execute('CREATE TABLE forums (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              rubrik TEXT NOT NULL)')
+  db.execute('CREATE TABLE messages (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              forum INTEGER NOT NULL,
+              user INTEGER NOT NULL,
+              message TEXT)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO users (username, pwd_digest, lvl) VALUES ("Pinkfix", "$2a$12$LIIaHcnjJk5qSQNOm6GI/uf.YdBm/xMEF5siJkq0vtE14McwKzKYS", true)')
-  #db.execute('INSERT INTO forums (name) VALUES ("Återinför Bräckelinjen!!!1!!1!1!!11!!")')
+  db.execute('INSERT INTO users (username, pwd_digest, lvl) VALUES ("Pinkfix", "$2a$12$LIIaHcnjJk5qSQNOm6GI/uf.YdBm/xMEF5siJkq0vtE14McwKzKYS", 1)')
+  db.execute('INSERT INTO users (username, pwd_digest, lvl) VALUES ("Jimmy Pandan", "$2a$12$ktRKfwRBuFfKaWZelPCh5OtnR5UlwHeye4cRiSUm/YrG5.eTQOpdq", 0)')
+  db.execute('INSERT INTO forums (rubrik) VALUES ("Återinför Bräckelinjen!")')
+  db.execute('INSERT INTO messages (forum,user,message) VALUES (1,2,"Jaa, den var banger!!!")')
 end
-
+# Personer
+# Pinkfix, Gargamel
+# Jimmy Pandan, SourBELTS
 
 seed!(db)
 
