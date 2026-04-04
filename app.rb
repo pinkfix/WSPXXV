9c0e378eee6565929aa2bb456abd37413dcebc89
pinkfix <robin@willim.se>
2026-04-04 15:16:42 +0200

    Ett gedigert försök att dokumentera enligt YARDoc

--- app.rb
+++ app.rb
@@ -12,6 +12,11 @@ include Model
 
 # BEFORE-BLOCK
 
+# Kollar alltid innan man öppnar en sida relaterad till forum ifall man är inloggad, och kollar isåfall admin perms.
+#
+# @param [Boolean] :logged_in Ifall användaren är inloggad.
+# @param [Boolean] :user_id Användarens ID. Båda är från session.
+# @see Model#backgroundCheck
 before('/forum/*') do
   if session[:logged_in]
     session[:admin] = backgroundCheck(session[:user_id])
@@ -20,10 +25,21 @@ end
 
 # FUNKTIONER RELATERADE TILL ANVÄNDARSYSTEMET
 
+# Visar registreringssidan.
+#
 get('/account/register') do
   slim(:signup)
 end
 
+# Registrerar en användare, ifall alla värden var korrekt formatterade
+#
+# @param [String] "user" Användarnamn
+# @param [String] "pwd" Lösenordet
+# @param [String] "pwd_confirm" Lösenordet, igen
+# @see Model#ddosBuffer
+# @see Model#validateLength
+# @see Model#getUserInfo
+# @see Model#sättUppAnvändare
 post('/register') do
   user = params["user"]
   pwd = params["pwd"]
@@ -53,12 +69,20 @@ post('/register') do
   end
 end
 
+#Visar inloggningssidan
+#
 get('/account/login') do
   slim(:login)
 end
 
+# Loggar in en användare, ifall alla värden var korrekta.
+#
+# @param [String] "user" Användarnamn
+# @param [String] "pwd" Lösenordet
+# @see Model#ddosBuffer
+# @see Model#getUserInfo
+# @see Model#logIn
 post('/login') do
-  db = getDB("db/railed.db")
   user = params["user"]
   pwd = params["pwd"]
 
@@ -82,17 +106,30 @@ post('/login') do
   end
 end
 
+# Loggar ut en användare.
+#
+# @see Model#resetSession
 post('/logout') do
   resetSession()
   redirect('/hub')
 end
 
+# Visar upp sidan där man kan "radera" sin användare.
+#
+# @param [Integer] :user_id Användarens ID (tas från session)
+# @see Model#tedIdInfo
 get('/account/delete') do
   @username = getIdInfo(session[:user_id])["username"]
-  p @username
   slim(:accdelete)
 end
 
+# Raderar en användare, ifall hen skriver in sitt lösenord.
+#
+# @param [String] :pwd_confirm Lösenordet som användaren skriver in för att konfirmera.
+# @param [Integer] :user_id Användarens ID (tas från session)
+# @see Model#getIdInfo
+# @see Model#eraseUser
+# @see Model#resetSession
 post('/accdelete') do
   pwd_digest = getIdInfo(session[:user_id])["pwd_digest"]
   pwd_confirm = params[:pwd_confirm]
@@ -102,7 +139,7 @@ post('/accdelete') do
     redirect('/hub')
   else
     error("delete", "Inkorrekt lösenord!")
-    sleep(2)
+    sleep(5)
   end
 end
 
@@ -116,6 +153,9 @@ get('/hub') do
 end
 
 #TINDERRELATERADE GETS OCH POSTS
+
+# Detta är en testsida som används i syfte att ha en sida att hindra utloggade från att nå.
+#
 get('/tinder') do
   db = getDB("db/railed.db")
   @id = session[:user_id]
@@ -124,6 +164,13 @@ get('/tinder') do
 end
 
 #Forumrelaterade Gets & Posts
+
+# Visar alla forum som finns i ordning från äldst till nyast, samt placerar de favoriserade över de ofavoriserade.
+#
+# @param [Boolean] :logged_in Om användaren är inloggad eller ej
+# @see Model#getForumsFav
+# @see Model#getForumsNotFav
+# @see Model#getForumsAll
 get('/forum/hub') do
   if session[:logged_in]
     @favforumen = getForumsFav(session[:user_id])
@@ -135,6 +182,13 @@ get('/forum/hub') do
   slim(:forumhub)
 end
 
+# Visar alla meddelanden från ett specifikt forum, samt vem som skrev dem.
+#
+# @param [Integer] :id Forumets ID
+# @param [Integer] :user_id Användarens ID, från sessions (if applicable)
+# @see Model#getIdInfo
+# @see Model#getForumsFromId
+# @see Model#getChatHistory
 get('/forum/:id') do
   @id = params[:id]
   @user_name = getIdInfo(session[:user_id])
@@ -143,6 +197,11 @@ get('/forum/:id') do
   slim(:forum)
 end
 
+# Skapar ett forum som sparas i databasen, och redirectar till den.
+#
+# @param [String] :rub Forumets namn
+# @see Model#createForum
+# @see Model#getForumFromRubrik
 post('/forum/create') do
   rubr = params[:rub]
   createForum(rubr)
@@ -150,6 +209,12 @@ post('/forum/create') do
   redirect("/forum/#{id}")
 end
 
+#Skriver ett meddelande i ett forum, dvs sparar till messages.
+#
+# @param [Integer] :id Forumets ID
+# @param [String] :meddelande Meddelandet
+# @param [Integer] :user_id Användarens ID, från sessions.
+# @see Model#insertMessages
 post('/chatta/:id') do
   id = params[:id]
   mess = params[:meddelande]
@@ -158,6 +223,11 @@ post('/chatta/:id') do
   redirect("/forum/#{id}")
 end
 
+# Raderar ett meddelande i ett forum, dvs tar bort från messages.
+#
+# @param [Integer] :forum_id Forumets ID
+# @param [Integer] :id Meddelandets ID
+# @see Model#deleteMessage
 post('/radera/:forum_id/:id') do
   id = params[:id]
   deleteMessage(id)
@@ -165,15 +235,23 @@ post('/radera/:forum_id/:id') do
   redirect("/forum/#{forum_id}")
 end
 
+# Favoriserar ett forum för en viss användare.
+#
+# @param [Integer] :forumet_id Forumets ID
+# @param [Integer] :user_id Användarens ID, från session
+# @see Model#favourite
 post('/forum/fav') do
   favourite(params[:forumet_id], session[:user_id])
   redirect('/forum/hub')
 end
 
+# Avfavoriserar ett forum för en viss användare.
+#
+# @param [Integer] :forumet_id Forumets ID
+# @param [Integer] :user_id Användarens ID, från session
+# @see Model#unfavourite
 post('/forum/avfav') do
   fid = params[:forumet_id]
-  p session[:user_id]
-  p fid
   unfavourite(fid, session[:user_id])
   redirect('/forum/hub')
 end
--- model.rb
+++ model.rb
@@ -1,146 +1,265 @@
 module Model
-
-#
-def getDB(database)
-  db = SQLite3::Database.new(database)
-  db.results_as_hash = true
-  return db
-end
-
-def backgroundCheck(user_id)
-  db = getDB("db/railed.db")
-  if db.execute("SELECT lvl FROM users WHERE id=?",user_id).first["lvl"] == 1
-    return true
+  # Links variable to a Database for easy use of the .execute()-function.
+  #
+  # @param [String] database href to the database.
+  #
+  # @return [Function] a function calling on the Database.
+  def getDB(database)
+    db = SQLite3::Database.new(database)
+    db.results_as_hash = true
+    return db
   end
-  return false
-end
 
-def error(reg_login_delete, error_message)
-  session[:error] = error_message
-  redirect('/account/#{reg_login_delete}')
-end
-
-
-def validateLength(username, password)
-  if username.length < 3
-    error("register","För kort användarnamn. Måste vara mellan 3 och 30 karaktärer")
-  elsif username.length > 30
-    error("register","För långt användarnamn. Måste vara mellan 3 och 30 karaktärer")
+  # Verifies that the person inserted is an admin.
+  #
+  # @param [integer] user_id The ID of the user
+  #
+  # @return [true] If user is admin
+  # @return [false] If user is not admin
+  def backgroundCheck(user_id)
+    db = getDB("db/railed.db")
+    if db.execute("SELECT lvl FROM users WHERE id=?",user_id).first["lvl"] == 1
+      return true
+    end
+    return false
   end
-  if password.to_i == password
-    error("register","Lösenord får ej bestå av endast siffror!")
-  elsif password.length < 8
-    error("register","För kort lösenord. Måste vara mellan 8 och 40 karaktärer")
-  elsif password.length > 40
-    error("register","För långt lösenord. Måste vara mellan 8 och 40 karaktärer")
+
+  # DRY-function saving the reason for error and then throwing the user back to the appropriate site.
+  #
+  # @param [String] reg_login_delete Which of the error sites to redirect to.
+  # @param [String] error_message The error message
+  #
+  # @return [String] error message and redirect to appropriate site.
+  def error(reg_login_delete, error_message)
+    session[:error] = error_message
+    redirect('/account/#{reg_login_delete}')
   end
-end
 
-def sättUppAnvändare(user, pwd, lvl)
-  db = getDB("db/railed.db")
-  pwd_digest = BCrypt::Password.create(pwd)
-  session[:logged_in] = true
-  user_id = db.execute("SELECT * FROM users").length + 1
-  session[:user_id] = user_id
-  session[:error] = "W"
-  db.execute('INSERT INTO users (username, pwd_digest, lvl) VALUES (?,?,?)',[user, pwd_digest,lvl])
-end
-
-def getIdInfo(id)
-  db = getDB("db/railed.db")
-  return db.execute("SELECT * FROM users WHERE id=?",id).first
-end
-
-def getUserInfo(user)
-  db = getDB("db/railed.db")
-  return db.execute("SELECT * FROM users WHERE username=?",user).first
-end
-
-def logIn(admin)
-  if admin == 1
-    session[:admin] = true
-  else
-    session[:admin] = false
-  end
-  session[:logged_in] = true
-end
-
-def getForumsFromID(id)
-  db = getDB("db/railed.db")
-  return db.execute("SELECT rubrik FROM forums WHERE id=?",@id).first
-end
-
-def getChatHistory(id)
-  db = getDB("db/railed.db")
-  return db.execute("SELECT messages.id,username,message FROM messages LEFT JOIN users on messages.user = users.id WHERE forum = ?", @id)
-end
-
-def createForum(forum_name)
-  db = getDB("db/railed.db")
-  db.execute("INSERT INTO forums (rubrik) VALUES (?)", forum_name)
-end
-
-def getForumFromRubrik(forum_name)
-  db = getDB("db/railed.db")
-  return db.execute("SELECT * FROM forums WHERE rubrik = ?", rubr).first
-end
-
-def insertMessage(forum, user, message)
-  db = getDB("db/railed.db")
-  db.execute('INSERT INTO messages (forum,user,message) VALUES (?,?,?)',[forum, user, message])
-end
-
-def deleteMessage(id)
-  db = getDB("db/railed.db")
-  db.execute("DELETE FROM messages WHERE id = ?",id)
-end
-
-def resetSession()
-  session[:logged_in] = false
-  session[:admin] = false
-  session[:user_id] = 0
-end
-
-def eraseUser(user_id)
-  db = getDB("db/railed.db")
-  name = getIdInfo(user_id)["username"]
-  db.execute("UPDATE users SET username='Deleted User',pwd_digest=? WHERE id=?", [name, user_id])
-end
-
-def ddosBuffer()
-  if session[:time]
-    time = Time.now
-    if time - session[:time] < 3
-      sleep(3)
+  # validates the length of the usernamne and password(and if the password is only numbers) when registering.
+  #
+  # @param [String] username Username
+  # @param [String] password The Password
+  def validateLength(username, password)
+    if username.length < 3
+      error("register","För kort användarnamn. Måste vara mellan 3 och 30 karaktärer")
+    elsif username.length > 30
+      error("register","För långt användarnamn. Måste vara mellan 3 och 30 karaktärer")
+    end
+    if password.to_i == password
+      error("register","Lösenord får ej bestå av endast siffror!")
+    elsif password.length < 8
+      error("register","För kort lösenord. Måste vara mellan 8 och 40 karaktärer")
+    elsif password.length > 40
+      error("register","För långt lösenord. Måste vara mellan 8 och 40 karaktärer")
     end
   end
-  session[:time] = Time.now
-end
 
-def getForumsFav(userid)
-  db = getDB("db/railed.db")
-  everything = db.execute('SELECT * FROM forums LEFT JOIN favoriter ON forums.id = favoriter.forum_id WHERE favoriter.user_id = ?', userid)
-  return everything
-end
+  # Sätter upp alla viktiga variabler för en inloggad användare, samt sätter in dessa i databasen
+  #
+  # @param [String] user Username
+  # @param [String] pwd Password
+  # @param [Boolean] lvl Admin status
+  def sättUppAnvändare(user, pwd, lvl)
+    db = getDB("db/railed.db")
+    pwd_digest = BCrypt::Password.create(pwd)
+    session[:logged_in] = true
+    user_id = db.execute("SELECT * FROM users").length + 1
+    session[:user_id] = user_id
+    session[:error] = "W"
+    db.execute('INSERT INTO users (username, pwd_digest, lvl) VALUES (?,?,?)',[user, pwd_digest,lvl])
+  end
 
-def getForumsNotFav(userid)
-  db = getDB("db/railed.db")
-  everything = db.execute('SELECT * FROM forums LEFT JOIN favoriter ON forums.id = favoriter.forum_id WHERE NOT favoriter.user_id = ? OR favoriter.user_id IS NULL', userid)
-  return everything
-end
+  # Hämtar all sparad info från användaren med ett specifikt ID.
+  #
+  # @param [Integer] id Användarens ID
+  #
+  # @return [Hash]
+  #   :id [Integer] Användarens ID
+  #   :username [String] Användarens namn
+  #   :pwd_digest [String] Andvändarens krypterade lösenord.
+  #   :lvl [Boolean] Om användaren är en Admin eller ej.
+  def getIdInfo(id)
+    db = getDB("db/railed.db")
+    return db.execute("SELECT * FROM users WHERE id=?",id).first
+  end
 
-def getForumsAll()
-  db = getDB("db/railed.db")
-  everything = db.execute('SELECT * FROM forums LEFT JOIN favoriter ON forums.id = favoriter.forum_id')
-  return everything
-end
+  # Hämtar all sparad info från användaren med ett specifikt namn.
+  #
+  # @param [String] user Användarens namn
+  #
+  # @return [Hash]
+  #   :id [Integer] Användarens ID
+  #   :username [String] Användarens namn
+  #   :pwd_digest [String] Andvändarens krypterade lösenord.
+  #   :lvl [Boolean] Om användaren är en Admin eller ej.
+  def getUserInfo(user)
+    db = getDB("db/railed.db")
+    return db.execute("SELECT * FROM users WHERE username=?",user).first
+  end
 
-def favourite(f_id, u_id)
-  db = getDB("db/railed.db")
-  db.execute("INSERT INTO favoriter (forum_id, user_id) VALUES (?,?)",[f_id,u_id])
-end
+  # Sätter session-variablerna :admin och :logged_in efter en lyckad inloggning.
+  #
+  # @param [Boolean] admin Ifall personen är admin
+  def logIn(admin)
+    if admin == 1
+      session[:admin] = true
+    else
+      session[:admin] = false
+    end
+    session[:logged_in] = true
+  end
 
-def unfavourite(f_id, u_id)
-  db = getDB("db/railed.db")
-  db.execute("DELETE FROM favoriter WHERE forum_id=? AND user_id=?",[f_id,u_id])
+  # Hämtar rubriken info från forumet med ett specifikt ID.
+  #
+  # @param [Integer] id Forumets ID
+  #
+  # @return [Hash]
+  #   :rubrik [String] Forumets namn
+  def getForumsFromID(id)
+    db = getDB("db/railed.db")
+    return db.execute("SELECT rubrik FROM forums WHERE id=?",@id).first
+  end
+
+  # Hämtar all relevant info från meddelanden i ett specifikt forum
+  #
+  # @param [Integer] id Forumets ID
+  #
+  # @return [Hash]
+  #   :id [Integer] meddelandets ID
+  #   :username [String] Användaren som skrev meddelandet
+  #   :message [String] Meddelandet
+  def getChatHistory(id)
+    db = getDB("db/railed.db")
+    return db.execute("SELECT messages.id,username,message FROM messages LEFT JOIN users on messages.user = users.id WHERE forum = ?", @id)
+  end
+
+  # Skapar ett forum i databastabellen forums
+  #
+  # @param [String] forum_name Forumets namn
+  def createForum(forum_name)
+    db = getDB("db/railed.db")
+    db.execute("INSERT INTO forums (rubrik) VALUES (?)", forum_name)
+  end
+
+  # Hämtar all info från forumet med ett specifikt namn.
+  #
+  # @param [String] forum_name Forumets rubrik
+  #
+  # @return [Hash]
+  #   :id [Integer] Forumets ID
+  #   :rubrik [String] Forumets namn
+  def getForumFromRubrik(forum_name)
+    db = getDB("db/railed.db")
+    return db.execute("SELECT * FROM forums WHERE rubrik = ?", rubr).first
+  end
+
+  # Lägger in ett meddelande i databasen.
+  #
+  # @param [Integer] forum Forumets ID
+  # @param [Integer] user Användarens ID
+  # @param [String] message Meddelandet som ska publiceras.
+  def insertMessage(forum, user, message)
+    db = getDB("db/railed.db")
+    db.execute('INSERT INTO messages (forum,user,message) VALUES (?,?,?)',[forum, user, message])
+  end
+
+  # Raderar ett meddelande.
+  #
+  # @param [Integer] id Meddelandets ID
+  def deleteMessage(id)
+    db = getDB("db/railed.db")
+    db.execute("DELETE FROM messages WHERE id = ?",id)
+  end
+
+  # Nollställer samtliga variabler i session.
+  #
+  def resetSession()
+    session[:logged_in] = false
+    session[:admin] = false
+    session[:user_id] = 0
+  end
+
+  # Raderar en användare. DISCLAIMER: Raderar den faktiskt inte! Ersätter istället namnet med Deleted User och det krypterade lösenordet med Namnet(for admin puropses).
+  #
+  # @param [Integer] user_id Användarens ID
+  # @see Model#getIDInfo
+  def eraseUser(user_id)
+    db = getDB("db/railed.db")
+    name = getIdInfo(user_id)["username"]
+    db.execute("UPDATE users SET username='Deleted User',pwd_digest=? WHERE id=?", [name, user_id])
+  end
+
+  # Pausar programmet i 3 sekunder ifall någon specifik person försökt att logga in mindre än 3 sekunder efter sitt tidigare försök.
+  #
+  def ddosBuffer()
+    if session[:time]
+      time = Time.now
+      if time - session[:time] < 3
+        sleep(3)
+      end
+    end
+    session[:time] = Time.now
+  end
+
+  # hämtar alla forum som en användare har gillat.
+  #
+  # @param [Integer] userid Användarens ID.
+  #
+  # @return [Hash]
+  #   :id [Integer] Forumets ID
+  #   :rubrik [String] Forumets namn
+  #   :forum_id [Integer] Forumets ID (Från relationstabellen), samma som den första.
+  #   :user_id [Integer] Användarens ID (Från relationstabellen)
+  def getForumsFav(userid)
+    db = getDB("db/railed.db")
+    everything = db.execute('SELECT * FROM forums LEFT JOIN favoriter ON forums.id = favoriter.forum_id WHERE favoriter.user_id = ?', userid)
+    p everything
+    return everything
+  end
+
+  # hämtar alla forum som en användare INTE har gillat.
+  #
+  # @param [Integer] userid Användarens ID.
+  #
+  # @return [Hash]
+  #   :id [Integer] Forumets ID
+  #   :rubrik [String] Forumets namn
+  #   :forum_id [Integer] Forumets ID (Från relationstabellen), samma som den första.
+  #   :user_id [Integer] Användarens ID (Från relationstabellen)
+  def getForumsNotFav(userid)
+    db = getDB("db/railed.db")
+    everything = db.execute('SELECT * FROM forums LEFT JOIN favoriter ON forums.id = favoriter.forum_id WHERE NOT favoriter.user_id = ? OR favoriter.user_id IS NULL', userid)
+    return everything
+  end
+
+  # hämtar alla forum (används när användaren inte är inloggad och därför inte har något ID).
+  #
+  # @return [Hash]
+  #   :id [Integer] Forumets ID
+  #   :rubrik [String] Forumets namn
+  #   :forum_id [Integer] Forumets ID (Från relationstabellen), samma som den första.
+  #   :user_id [Integer] Användarens ID (Från relationstabellen)
+  def getForumsAll()
+    db = getDB("db/railed.db")
+    everything = db.execute('SELECT * FROM forums LEFT JOIN favoriter ON forums.id = favoriter.forum_id')
+    return everything
+  end
+
+  # Lägger till en koppling i relationstabellen favoriter.
+  #
+  # @param [Integer] f_id Forumets ID
+  # @param [Integer] u_id Användarens ID
+  def favourite(f_id, u_id)
+    db = getDB("db/railed.db")
+    db.execute("INSERT INTO favoriter (forum_id, user_id) VALUES (?,?)",[f_id,u_id])
+  end
+
+  # Tar bort en koppling i relationstabellen favoriter.
+  #
+  # @param [Integer] f_id Forumets ID
+  # @param [Integer] u_id Användarens ID
+  def unfavourite(f_id, u_id)
+    db = getDB("db/railed.db")
+    db.execute("DELETE FROM favoriter WHERE forum_id=? AND user_id=?",[f_id,u_id])
+  end
 end
